150cf10df87ad364ff86131e3cc66ee800b909f2
FelixofRivia <filippomei99@gmail.com>
2025-04-08 18:31:11 +0200

    grain data structures for detector response in/out

--- include/grain/digi.h
+++ include/grain/digi.h
@@ -1,12 +1,29 @@
 #pragma once
 
 #include <ufw/data.hpp>
+#include <vector>
+#include <cstdint>
 
 namespace sand::grain {
 
   struct digi : ufw::data::base<ufw::data::managed_tag, ufw::data::instanced_tag, ufw::data::context_tag> {
+    struct channel:
+    {
+       uint16_t channel_id;
+       double time_rising_edge;
+       double time_over_threshold;
+       double charge;
+       std::vector<uint32_t> h_indices; 
+    };
 
+    struct camera:
+    {
+      uint16_t camera_id;  // or std::string camera_name;
+      std::vector<channel> channels;
+    };
   };
+
+  using digitized_image = std::vector<camera>;
 
 }
 
--- include/grain/photons.h
+++ include/grain/photons.h
@@ -1,13 +1,38 @@
 #pragma once
 
 #include <ufw/data.hpp>
+#include <vector>
+#include <cstdint>
 
 namespace sand::grain {
 
   struct photons : ufw::data::base<ufw::data::managed_tag, ufw::data::instanced_tag, ufw::data::context_tag> {
+    struct pe
+    {
+      uint32_t h_index;
+      double energy;
+      double time;
+      double x;
+      double y;
+      double z;
+      double x_origin;
+      double y_origin;
+      double z_origin;
+      double px;
+      double py;
+      double pz;
+      double scatter;
+      bool inside_camera;
+    };
 
+    struct camera:
+    {
+      uint16_t camera_id;  // or std::string camera_name;
+      std::vector<pe> photoelectrons;
+    };
   };
 
+  using optical_image = std::vector<camera>;
 }
 
 UFW_DECLARE_MANAGED_DATA(sand::grain::photons)
