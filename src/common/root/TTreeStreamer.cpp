21d7bf521a5506b2d3ad711823e234179239ca08
ntosi <nicolo.tosi@bo.infn.it>
2025-04-13 16:09:44 +0200

    Ported to new streamer API

--- src/common/root/TTreeStreamer.cpp
+++ src/common/root/TTreeStreamer.cpp
@@ -23,61 +23,54 @@ namespace sand::common::root {
     delete m_file;
   }
 
-  void TTreeStreamer::configure(const ufw::config& cfg) {
-    streamer::configure(cfg);
-    std::string openmode = cfg.value("mode", "READ");
-    if (openmode == "READ") {
-      m_mode = iop::ro;
-    } else if (openmode == "RECREATE" || openmode == "CREATE" || openmode == "NEW") {
-      m_mode = iop::wo;
-    } else if (openmode == "UPDATE") {
-      m_mode = iop::rw;
-    } else {
-      UFW_ERROR("Mode {} is not supported by TTreeStreamer", openmode);
+  void TTreeStreamer::configure(const ufw::config& cfg, const ufw::type_id& tp, ufw::op_type op) {
+    streamer::configure(cfg, tp, op);
+    TClass* tcl = TClass::GetClass(tp.c_str());
+    if (!tcl) {
+      UFW_ERROR("TClass for '{}' not found.", tp);
     }
-    m_file = new TFile(path().c_str(), openmode.c_str());
+    const char* opmode = "";
+    switch (op) {
+    case ufw::op_type::ro:
+      opmode = "READ";
+      break;
+    case ufw::op_type::wo:
+      opmode = "RECREATE";
+      break;
+    case ufw::op_type::rw:
+      opmode = "UPDATE";
+      break;
+    default:
+      UFW_ERROR("Mode {} is not supported by TTreeStreamer", op);
+      break;
+    };
+    m_file = new TFile(path().c_str(), opmode);
     if (!m_file->IsOpen() || m_file->IsZombie()) {
       UFW_ERROR("File {} could not be opened", path().string());
     }
     std::string treename = cfg.at("tree");
-    if (m_mode == iop::ro || m_mode == iop::rw) {
+    if (op & ufw::op_type::ro) {
       m_tree = m_file->Get<TTree>(treename.c_str());
     } else {
       m_tree = new TTree(treename.c_str(), "");
     }
   }
 
-  void TTreeStreamer::attach(const ufw::type_id& t, ufw::data::data_base& d) {
-    UFW_DEBUG("Attached object at {} of type {}", fmt::ptr(&d), t);
-    TClass* tcl = TClass::GetClass(t.c_str());
-    if (!tcl) {
-      UFW_ERROR("TClass for '{}' not found.", t);
-    }
+  void TTreeStreamer::attach(ufw::data::data_base& d) {
     //you would think using a temporary here would be fine, and yet...
     m_branchaddr = &d;
     //remove any namespace from the branch name, for convenience.
-    const char* brname = t.c_str();
-    auto idx = t.find_last_of(':');
-    if (idx != std::string::npos) {
-      brname += idx + 1;
-    }
-    if (m_mode == iop::ro || m_mode == iop::rw) {
-      TBranch* br = m_tree->GetBranch(brname);
+    auto brname = ufw::simplified_name(type());
+    if (operation() & ufw::op_type::ro) {
+      TBranch* br = m_tree->GetBranch(brname.c_str());
       if (!br) {
         UFW_ERROR("TBranch '{}' not found.", brname);
       }
       br->SetAddress(&m_branchaddr);
-    } else if (m_mode == iop::wo) {
-      m_tree->Branch(brname, t.c_str(), &m_branchaddr);
+    } else if (operation() == ufw::op_type::wo) {
+      m_tree->Branch(brname.c_str(), type().c_str(), &m_branchaddr);
     }
-  }
-
-  ufw::streamer::iop TTreeStreamer::support(const ufw::type_id& t) const {
-    if (ufw::data::is_complex_type(t) || ufw::data::is_global_type(t)) {
-      return iop::none;
-    }
-    TClass* tcl = TClass::GetClass(t.c_str());
-    return tcl ? m_mode : iop::none;
+    streamer::attach(d);
   }
 
   void TTreeStreamer::read(ufw::context_id i) {
