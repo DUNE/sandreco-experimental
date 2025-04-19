9b31f622e677a7cfc877f4aca141e198f50afc18
ntosi <nicolo.tosi@bo.infn.it>
2025-04-19 22:11:28 +0200

    Ported to new API

--- src/processes/grain/time_slicing/time_slicing.cpp
+++ src/processes/grain/time_slicing/time_slicing.cpp
@@ -7,53 +7,40 @@
 #include <grain/digi.h>
 #include <grain/picture.h>
 
+namespace sand::grain {
 
-class time_slicing : public ufw::process {
+  class time_slicing : public ufw::process {
 
   public:
     time_slicing();
     void configure (const ufw::config& cfg) override;
-    // const ufw::var_type_map& products() const override;
-    // const ufw::var_type_map& requirements() const override;
-    void run(const ufw::var_id_map& inputs, const ufw::var_id_map& outputs) override;
+    void run() override;
 
-  
   private:
     int m_seed = 0;
-};
-  
-  UFW_REGISTER_PROCESS(time_slicing)
-  UFW_REGISTER_DYNAMIC_PROCESS_FACTORY(time_slicing)
+  };
 
   void time_slicing::configure (const ufw::config& cfg) {
     process::configure(cfg);
   }
-  
-  // ufw::data_list detector_response::products() const {
-  //   return ufw::data_list{{"output", "sand::example"}};
-  // }
-  
-  // ufw::data_list detector_response::requirements() const {
-  //   return ufw::data_list{{"input", "sand::example"}};
-  // }
-  // time_slicing::time_slicing() : process({{"cameras_in", "sand::grain::digi"}}, {{"sliced_pictures_out", "sand::grain::pictures"}}) {
-  time_slicing::time_slicing() : process({{"cameras_in", "sand::grain::digi"}}, {}) {
+
+  time_slicing::time_slicing() : process({{"digi", "sand::grain::digi"}}, {}) {
     UFW_INFO("Creating a time_slicing process at {}", fmt::ptr(this));
   }
 
+  void time_slicing::run() {
+    const auto& digis = get<sand::grain::digi>("digi");
+    UFW_INFO("Camera images size: {}.", digis.images.size());
 
-  void time_slicing::run(const ufw::var_id_map& inputs, const ufw::var_id_map& outputs) {
-    auto& hits = ufw::context::instance<sand::grain::digi>(inputs.at("cameras_in"));
-    UFW_INFO("QUI: {}", hits.images.size());
-    
-    UFW_INFO("Executing test_process::run at {} with parameters:", fmt::ptr(this));
-    for (const auto& [var, id]: inputs) {
-      UFW_INFO("  in : {{{}, {}}}", var, id);
-    }
-    for (auto& i : hits.images) {
+    for (auto& i : digis.images) {
       UFW_INFO("Image {}", i.camera_id);
       for (auto& c : i.channels) {
         // UFW_INFO("Position: {}", p.pos.X());
       }
     }
-}  
+  }
+
+}
+  
+UFW_REGISTER_PROCESS(sand::grain::time_slicing)
+UFW_REGISTER_DYNAMIC_PROCESS_FACTORY(sand::grain::time_slicing)
