70a804a6bfff3fa0a4bbf0d67a795308991538ff
ntosi <nicolo.tosi@bo.infn.it>
2025-04-20 00:41:55 +0200

    Added stub for truth access

--- include/common/truth.h
+++ include/common/truth.h
@@ -1,15 +1,38 @@
 #pragma once
 
+#ifndef __CLING__
 #include <ufw/index.hpp>
+
+class TG4HitSegment;
+
+namespace sand {
+
+  extern const TG4HitSegment& get_truth_at(const size_t& i);
+  extern bool check_truth_at(const size_t& i);
+
+  using truth_adapter = ufw::data::collection_adapter<const TG4HitSegment, size_t, get_truth_at, check_truth_at>;
+  using truth_index = ufw::data::index<truth_adapter>;
+
+}
+
+#endif //__CLING__
 
 namespace sand {
 
   struct true_hit {
-    int hit;
+#ifdef __CLING__
+    size_t hit;
+#else //__CLING__
+    truth_index hit;
+#endif //__CLING__
   };
 
   struct true_hits {
-    std::vector<int> hits;
+#ifdef __CLING__
+    std::vector<size_t> hits;
+#else //__CLING__
+    std::vector<truth_index> hits;
+#endif //__CLING__
   };
 
 }
