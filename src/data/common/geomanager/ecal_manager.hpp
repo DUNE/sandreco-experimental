da7adb3c8bd8f6ca8d259671be3e79ec6bae043d
Nicolò Tosi <ntosi@bo.infn.it>
2025-05-09 18:05:45 +0200

    ecal interface

--- src/data/common/geomanager/ecal_manager.hpp
+++ src/data/common/geomanager/ecal_manager.hpp
@@ -7,7 +7,26 @@ namespace sand {
   class geomanager::ecal_manager : public subdetector_manager {
 
   public:
+    enum class cell_type {
+      barrel, endcap
+    };
+
+    struct cell {
+      guid id;
+      pos_3d centre;
+      double length;
+      cell_type type;
+    };
+
+  public:
     ecal_manager(const geomanager&);
+
+    const cell& at(pos_3d);
+
+    const cell& at(guid);
+
+  private:
+    std::map<guid, cell> m_cells;
 
   };
 
