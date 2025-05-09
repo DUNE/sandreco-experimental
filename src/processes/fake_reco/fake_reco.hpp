e61ed9688f3f1c3a4e80d8908000bf21c493bb6a
Pe13 <paolo.peforni@gmail.com>
2025-05-09 15:06:14 +0000

    First implementation

---
+++ src/processes/fake_reco/fake_reco.hpp
@@ -0,0 +1,20 @@
+//
+// Created by paolo on 08/05/2025.
+//
+
+#ifndef FAKE_RECO_HPP
+#define FAKE_RECO_HPP
+
+#include <ufw/process.hpp>
+
+namespace sand::fake_reco {
+    class fake_reco : public ufw::process {
+    public:
+        fake_reco();
+        void run() override;
+    };
+} // namespace sand::fake_reco
+
+UFW_REGISTER_PROCESS(sand::fake_reco::fake_reco);
+
+#endif //FAKE_RECO_HPP
