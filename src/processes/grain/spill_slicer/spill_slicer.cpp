fc597a4b8ba5709e943adeee23ef91f31f9082dc
FelixofRivia <filippomei99@gmail.com>
2025-05-15 13:09:36 +0200

    leave manual slicing option and test

--- src/processes/grain/spill_slicer/spill_slicer.cpp
+++ src/processes/grain/spill_slicer/spill_slicer.cpp
@@ -10,6 +10,7 @@
 #include <array>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 namespace sand::grain {
 
@@ -19,29 +20,40 @@ namespace sand::grain {
     spill_slicer();
     void configure (const ufw::config& cfg) override;
     void run() override;
-    std::vector<double> compute_slice_times();
 
   private:
-    int m_seed = 0;
-    uint32_t m_min_response_signal;
+    double m_min_response_signal;
     double m_delta_ns_for_comparison;
-    //std::vector<double> m_slice_times;
     uint64_t m_stat_photons_processed;
     uint64_t m_stat_photons_accepted;
     uint64_t m_stat_photons_discarded;
+    std::vector<double> m_slice_times;
+    int m_seed = 0;
+    bool m_use_algo;
+
+    void compute_slice_times();
     
   };
 
   void spill_slicer::configure (const ufw::config& cfg) {
     process::configure(cfg);
-    // for (auto time: cfg.at("slice_times") ) {
-    //   m_slice_times.push_back(time);
-    // }
-    m_min_response_signal = cfg.at("min_response_signal");
-    m_delta_ns_for_comparison = cfg.at("delta_ns_for_comparison");
+    m_slice_times.clear();
+    for (auto time: cfg.value("slice_times", m_slice_times)) {
+      m_slice_times.push_back(time);
+    }
+    if (m_slice_times.size() > 1) { // At least 2 edges for 1 slice
+      UFW_DEBUG("Using slice times from parameters");
+      m_use_algo = false;
+    }
+    else {
+      UFW_DEBUG("Using slicing algorithm");
+      m_use_algo = true;
+      m_min_response_signal = cfg.at("min_response_signal");
+      m_delta_ns_for_comparison = cfg.at("delta_ns_for_comparison");
+    }
   }
 
-  std::vector<double> spill_slicer::compute_slice_times() {
+  void spill_slicer::compute_slice_times() {
     // Place times into bins
     const int n_bins{100};
     const double min_time{0.0};
@@ -56,28 +68,28 @@ namespace sand::grain {
       for (auto& pe : cam.photoelectrons) {
         double time{pe.time_rising_edge};
         if (time >= min_time && time < max_time) {
-          size_t bin_index = static_cast<size_t>((time - min_time) / bin_width);
+          size_t bin_index = static_cast<size_t>(std::floor((time - min_time) / bin_width));
           binned_times[bin_index] += pe.charge;
         }
         else {
-          UFW_DEBUG("Digi in camera {} {} is out of time window for slicing (t = {} ns)", cam.camera_id, cam.camera_name, time);
+          UFW_WARN("Digi in camera {} {} is out of time window for slicing (t = {} ns)", cam.camera_id, cam.camera_name, time);
         }
       }
     }
 
     // Go through bins to find slice_edges
     const size_t n_close_bins = static_cast<size_t>(m_delta_ns_for_comparison / bin_width);
-    std::vector<double> slice_edges;
-    slice_edges.push_back(min_time);
+    m_slice_times.clear();
+    m_slice_times.push_back(min_time);
     for (size_t i = n_close_bins; i < n_bins - n_close_bins; ++i) {
-        uint64_t left_max = *std::max_element(binned_times.begin() + i - n_close_bins, binned_times.begin() + i);
-        uint64_t right_max = *std::max_element(binned_times.begin() + i + 1, binned_times.begin() + i + n_close_bins + 1);
-        if (binned_times[i] > left_max && binned_times[i] > right_max && binned_times[i] >= m_min_response_signal) {
-            slice_edges.push_back(static_cast<double>(i) * bin_width);
-        }
+      auto center = binned_times.begin() + i;
+      uint64_t left_max = *std::max_element(center - n_close_bins, center);
+      uint64_t right_max = *std::max_element(center + 1, center + n_close_bins + 1);
+      if (binned_times[i] > left_max && binned_times[i] > right_max && binned_times[i] >= m_min_response_signal) {
+          m_slice_times.push_back(static_cast<double>(i) * bin_width);
+      }
     }
-    slice_edges.push_back(max_time);
-    return slice_edges;
+    m_slice_times.push_back(max_time);
   }
 
   spill_slicer::spill_slicer() : process({{"digi", "sand::grain::digi"}}, {{"images", "sand::grain::images"}}) {
@@ -90,21 +102,24 @@ namespace sand::grain {
     m_stat_photons_discarded = 0;
     const auto& digis_in = get<digi>("digi");
     auto& images_out = set<images>("images");
-    std::vector<double> slice_times = compute_slice_times();
-    for (int img_idx = 0; img_idx < slice_times.size() - 1; img_idx++) {
-      UFW_INFO("Building images in time interval [{} - {}] ns", slice_times[img_idx], slice_times[img_idx + 1]);
+    if (m_use_algo) {
+      m_slice_times.clear();
+      compute_slice_times();
+    }
+    for (int img_idx = 0; img_idx < m_slice_times.size() - 1; img_idx++) {
+      UFW_INFO("Building images in time interval [{} - {}] ns", m_slice_times[img_idx], m_slice_times[img_idx + 1]);
       for (auto& cam : digis_in.cameras) {
         UFW_DEBUG("Camera {} {}", cam.camera_id, cam.camera_name);
         images::image cam_image;
         cam_image.blank();
         cam_image.camera_id = cam.camera_id;
         cam_image.camera_name = cam.camera_name;
-        cam_image.time_begin = slice_times[img_idx];
-        cam_image.time_end = slice_times[img_idx + 1];
+        cam_image.time_begin = m_slice_times[img_idx];
+        cam_image.time_end = m_slice_times[img_idx + 1];
         for (auto& pe : cam.photoelectrons) {
           //UFW_DEBUG("channel id: {}, time: {}", pe.channel_id, pe.time_rising_edge);
           m_stat_photons_processed++;
-          if (pe.time_rising_edge >= slice_times[img_idx] && pe.time_rising_edge < slice_times[img_idx + 1]) {
+          if (pe.time_rising_edge >= m_slice_times[img_idx] && pe.time_rising_edge < m_slice_times[img_idx + 1]) {
               //UFW_DEBUG("pe to be assigned to image {}", img_idx);
               //consistent indexing: Row Major
               cam_image.pixels.Array()[pe.channel_id].add(pe.hits);
