3e485d45db038c6e617b0f4e22b65683bf83e573
ntosi <nicolo.tosi@bo.infn.it>
2025-05-22 01:20:42 +0200

    cleanup of runmanager

--- src/data/grain/geant_run_manager/geant_run_manager.cpp
+++ src/data/grain/geant_run_manager/geant_run_manager.cpp
@@ -1,15 +1,9 @@
-#include <ufw/data.hpp>
-#include <ufw/utils.hpp>
-
-#include <G4RunManager.hh>
-#include "G4UImanager.hh"
-#include "G4GDMLParser.hh"
-#include "G4VisExecutive.hh"
-#include "G4UIExecutive.hh"
+#include <ufw/factory.hpp>
 
 #include <geant_run_manager.hpp>
 
 namespace sand::grain {
 
-geant_run_manager::geant_run_manager(const ufw::config&) {}
+  geant_run_manager::geant_run_manager(const ufw::config&) {}
+
 }
