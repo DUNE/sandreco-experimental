bdaaef4c023148e2ef391d711ac73e6a6177f70d
ntosi <nicolo.tosi@bo.infn.it>
2025-05-22 02:16:04 +0200

    used new gdml parser

--- src/data/common/geoinfo/geoinfo.cpp
+++ src/data/common/geoinfo/geoinfo.cpp
@@ -17,7 +17,8 @@ namespace sand {
 
   geoinfo::geoinfo(const ufw::config& cfg) {
     m_root_path = cfg.value("basepath", "/volWorld_PV/rockBox_lv_PV_0/volDetEnclosure_PV_0/volSAND_PV_0/MagIntVol_volume_PV_0/");
-    m_grain.reset(new grain_info(*this));
+    auto grain_path = cfg.at("grain_geometry");
+    m_grain.reset(new grain_info(*this, grain_path));
     m_ecal.reset(new ecal_info(*this));
     m_tracker.reset(new drift_info(*this));
     m_tracker.reset(new stt_info(*this));
--- src/data/common/geoinfo/grain_info.cpp
+++ src/data/common/geoinfo/grain_info.cpp
@@ -1,7 +1,53 @@
+#include <ufw/context.hpp>
 #include <grain_info.hpp>
+#include <geant_gdml_parser/geant_gdml_parser.hpp>
 
 namespace sand {
 
-  geoinfo::grain_info::grain_info(const geoinfo& gi) : subdetector_info(gi, "sand_inner_volume_PV_0/GRAIN_lv_PV_0/GRAIN_LAr_lv_PV_0") {}
+  namespace {
+
+    void print_auxlist(const G4GDMLAuxListType& list, std::string depth = "") {
+      for (const auto& item: list) {
+        UFW_DEBUG("{} Auxiliary '{}' = {} {}.", depth, item.type, item.value, item.unit);
+        if (item.auxList) {
+          print_auxlist(*item.auxList, depth += ' ');
+        }
+      }
+    };
+
+  }
+
+  geoinfo::grain_info::grain_info(const geoinfo& gi, const std::string& inner_geom) : subdetector_info(gi, "sand_inner_volume_PV_0/GRAIN_lv_PV_0/GRAIN_LAr_lv_PV_0") {
+    UFW_INFO("Reading grain geometry details from {}.", inner_geom);
+    auto& gdml = ufw::context::current()->instance<grain::geant_gdml_parser>(ufw::public_id(inner_geom));
+
+
+    UFW_DEBUG("Printing auxmap");
+    for (const auto& [vol, list]: *gdml.GetAuxMap()) {
+      UFW_DEBUG("Logical volume '{}' at {} has {} auxiliary info.", vol->GetName(), fmt::ptr(vol), list.size());
+      print_auxlist(list);
+    }
+    UFW_DEBUG("End of auxmap");
+  }
 
 }
+
+/*
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:25] Printing auxmap
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:27] Logical volume 'codedApertureMask' at 0x1677b1a0 has 1 auxiliary info.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]  Auxiliary 'Mask' = codedApertureMask .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'rank' = 31 .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'cellcount' = 61 .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'cellsize' = 2.710 mm.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'celledge' = 0.200 mm.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:27] Logical volume 'cam_volume' at 0x167af8a0 has 1 auxiliary info.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]  Auxiliary 'Camera' =  .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:27] Logical volume 'lar_volume' at 0x16877580 has 1 auxiliary info.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]  Auxiliary 'Fiducial' =  .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:27] Logical volume 'photoDetector' at 0x168d6dc0 has 1 auxiliary info.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]  Auxiliary 'Sensor' = S14160-6050HS .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'cellcount' = 32 .
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'cellsize' = 3.000 mm.
+[2025-05-22 00:14:02.425] [debug] [grain_info.cpp:11]   Auxiliary 'celledge' = 0.200 mm.
+[2025-05-22 00:14:02.425] [critical] [grain_info.cpp:30] End of auxmap
+*/
