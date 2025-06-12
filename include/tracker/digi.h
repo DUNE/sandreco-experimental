ebb29ae8a86a195a9b1f9436bd327944331df285
fbattist <federico.battisti@bo.infn.it>
2025-06-12 16:03:02 +0200

    Added signal structure to tracker digi

--- include/tracker/digi.h
+++ include/tracker/digi.h
@@ -10,10 +10,14 @@
 namespace sand::tracker {
 
   struct digi : public sand::true_hits, ufw::data::base<ufw::data::managed_tag, ufw::data::instanced_tag, ufw::data::context_tag> {
-    geo_id wire;
-    channel_id channel;
-    double tdc; 
-    double adc;
+    struct signal {
+      channel_id channel;  // The channel associated with the signal
+      double tdc;  // Time-to-digital converter value
+      double adc;  // Analog-to-digital converter value
+    };
+    
+    using signal_collection = std::vector<signal>;
+    signal_collection signals;  // Collection of signals associated with the digi
   };
 }
 
