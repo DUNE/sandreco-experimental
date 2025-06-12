8d2ee1d8c9df4663f27e540064fd1f9a616357cc
Nicolò Tosi <ntosi@bo.infn.it>
2025-06-12 15:46:10 +0200

    Added new 3D tracklet structure

---
+++ include/tracker/tracklet.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ufw/data.hpp>
+
+#include <sand.h>
+
+namespace sand::tracker {
+
+  struct tracklet_collection : ufw::data::base<ufw::data::managed_tag, ufw::data::instanced_tag, ufw::data::context_tag> {
+
+    struct tracklet {
+      pos_3d offset; ///< position of centre of tracklet, in global coordinates
+      double sigma_pos; ///< position error of centre of tracklet, 1-sigma radius around offset
+      dir_3d segment; ///< normalized direction of tracklet, in global coordinates
+      double sigma_dir; ///< direction error, 1-sigma half-angle of cone around segment
+      double quality; ///< fit quality, Chi^2/NDF
+    };
+
+    using tracklet_list = std::vector<tracklet>;
+    tracklet_list tracklets;
+
+  };
+
+}
+
+UFW_DECLARE_MANAGED_DATA(sand::tracker::tracklet_collection)
