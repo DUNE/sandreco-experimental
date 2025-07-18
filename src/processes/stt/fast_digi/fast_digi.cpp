168f6c39f72cfa3cf164a6af92a7c9296812053f
fbattist <federico.battisti@bo.infn.it>
2025-07-18 12:06:57 +0200

    Added test and process skeleton

--- src/processes/stt/fast_digi/fast_digi.cpp
+++ src/processes/stt/fast_digi/fast_digi.cpp
@@ -6,7 +6,7 @@
 
 #include <tracker/digi.h>
 
-namespace sand::tracker {
+namespace sand::stt {
 
   class fast_digi : public ufw::process {
 
@@ -22,6 +22,23 @@ namespace sand::tracker {
 
   };
 
+  void fast_digi::configure (const ufw::config& cfg) {
+    process::configure(cfg); 
+    m_drift_velocity = cfg.at("drift_velocity");
+    m_wire_velocity = cfg.at("wire_velocity");
+    m_sigma_tdc = cfg.at("sigma_tdc");
+  }
+
+  fast_digi::fast_digi() : process({}, {{"digi", "sand::tracker::digi"}}) {
+    UFW_DEBUG("Creating a fast_digi process at {}", fmt::ptr(this));
+  } 
+
+  void fast_digi::run() {
+    UFW_DEBUG("Running fast_digi process at {}", fmt::ptr(this));
+    
+  }
+
 }
 
-UFW_REGISTER_PROCESS(sand::tracker::fast_digi)
+UFW_REGISTER_PROCESS(sand::stt::fast_digi)
+UFW_REGISTER_DYNAMIC_PROCESS_FACTORY(sand::stt::fast_digi)
