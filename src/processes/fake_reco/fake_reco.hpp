ca978c5862f200ef0fef78602dfbddafbbafedea
Paolo Forni <paolo.peforni@gmail.com>
2025-09-26 12:26:58 +0000

    Fill a SRTrueInteraction and avoid usage of WIP utilities

--- src/processes/fake_reco/fake_reco.hpp
+++ src/processes/fake_reco/fake_reco.hpp
@@ -7,16 +7,27 @@
 
 #include <ufw/process.hpp>
 
-namespace sand::fake_reco {
-  class fake_reco : public ufw::process {
-  public:
-    fake_reco();
+#include <edep_reader/edep_reader.hpp>
 
-    void configure (const ufw::config& cfg) override;
-    void run() override;
-  };
+#include <duneanaobj/StandardRecord/StandardRecord.h>
+
+namespace sand::fake_reco {
+
+    caf::SRRecoParticle
+    reco_particle_from_edep_trajectory(const EDEPTrajectory& particle);
+
+    caf::SRTrueParticle
+    true_particle_from_edep_trajectory(const EDEPTrajectory& particle);
+
+        class fake_reco : public ufw::process {
+    public:
+        fake_reco();
+
+        void configure(const ufw::config& cfg) override;
+        void run() override;
+    };
 } // namespace sand::fake_reco
 
 UFW_REGISTER_PROCESS(sand::fake_reco::fake_reco);
 
-#endif //FAKE_RECO_HPP
+#endif // FAKE_RECO_HPP
