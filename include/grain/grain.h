6d14184944c250863b31d07fb8e24b01bd3ce692
Nicolò Tosi <ntosi@bo.infn.it>
2025-10-03 11:36:34 +0200

    Added voxel_array class

--- include/grain/grain.h
+++ include/grain/grain.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include <common/sand.h>
 
 namespace sand::grain {
@@ -19,6 +21,72 @@ namespace sand::grain {
     mura = 2,
     lens = 4,
     doped = 8,
+  };
+
+  using index_3d = ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<size_t>>;
+  using size_3d = ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<size_t>>;
+
+  template <typename T>
+  class voxel_array {
+
+    voxel_array(size_3d sz) :
+     m_data(new T[count(sz)]), m_size(sz) {}
+
+    voxel_array(size_3d sz, T init) :
+     m_data(new T[count(sz)]), m_size(sz) {
+      std::fill_n(data(), count(sz), init);
+    }
+
+    voxel_array(size_3d sz, const T* raw) :
+     m_data(new T[count(sz)]), m_size(sz) {
+      std::copy_n(raw, count(sz), data());
+    }
+
+    voxel_array(const voxel_array&) = delete;
+
+    voxel_array(voxel_array&&) = default;
+
+    voxel_array& operator = (const voxel_array&) = delete;
+
+    voxel_array& operator = (voxel_array&&) = default;
+
+    voxel_array clone() const {
+      return voxel_array(m_size, data());
+    }
+
+    T at(index_3d i) const {
+      return m_data[linear(i)];
+    }
+
+    T& at(index_3d i) {
+      return m_data[linear(i)];
+    }
+
+    const T* data() const {
+      return m_data.get();
+    }
+
+    T* data() {
+      return m_data.get();
+    }
+
+    index_3d index(size_t);
+
+    size_t linear(index_3d);
+
+    size_3d size() const {
+      return m_size;
+    }
+
+  private:
+    static size_t count(size_3d sz ) {
+      return sz[0] * sz[1] * sz[2];
+    }
+
+  private:
+    std::unique_ptr<T[]> m_data;
+    size_3d m_size;
+
   };
 
 }
