3ab2ce96a4b266aa5ce276bf4bf90fb7bb4a2a0c
fbattist <federico.battisti@bo.infn.it>
2025-10-27 15:55:41 +0100

    Fixed compilation info and cleaned up

--- src/processes/stt/fast_digi/fast_digi.cpp
+++ src/processes/stt/fast_digi/fast_digi.cpp
@@ -16,20 +16,6 @@
 
 namespace sand::stt {
 
-  // class fast_digi : public ufw::process {
-
-  // public:
-  //   fast_digi();
-  //   void configure (const ufw::config& cfg) override;
-  //   void run() override;
-
-  // private:
-  //   double m_drift_velocity; //[mm/ns]
-  //   double m_wire_velocity; //[mm/ns]
-  //   double m_sigma_tdc; //[ns]
-
-  // };
-
   void fast_digi::configure (const ufw::config& cfg) {
     process::configure(cfg); 
     m_drift_velocity = cfg.at("drift_velocity");
@@ -46,58 +32,17 @@ namespace sand::stt {
     UFW_DEBUG("Running fast_digi process at {}", fmt::ptr(this));
     const auto& tree = get<sand::edep_reader>();
     const auto& gi = get<geoinfo>();
-    //const auto* stt = dynamic_cast<const sand::geoinfo::stt_info*>(&gi.tracker());
     auto& digi = set<sand::tracker::digi>("digi");
     auto& tgm = ufw::context::current()->instance<root_tgeomanager>();
     std::map<geo_id, std::vector<EDEPHit>> hits_by_tube;
 
-    //UFW_DEBUG(" subdetector type is {}", static_cast<int>(gi.tracker().subdetector()));
     if(gi.tracker().subdetector() == subdetector_t::STT){
       UFW_DEBUG(" STT subdetector implementation");
-      group_hits_by_tube(hits_by_tube, gi, tree, tgm);
-      // auto* stt = dynamic_cast<const sand::geoinfo::stt_info*>(&gi.tracker());
-      // for (const auto& trj : tree) {
-      //   const auto& hit_map = trj.GetHitMap(); // pointer, not value    
-      //   if(hit_map.find(component::STRAW) == hit_map.end()) continue;
+      group_hits_by_tube(hits_by_tube, gi, tree, tgm);   
 
-      //   UFW_DEBUG("Found {} STRAW hits for trajectory with ID {}", hit_map.at(component::STRAW).size(), trj.GetId());
-    
-      //   for (const auto& hit : hit_map.at(component::STRAW)){
-
-      //     pos_3d hit_mid_point = pos_3d((hit.GetStart().X() + hit.GetStop().X()) * 0.5,
-      //                                   (hit.GetStart().Y() + hit.GetStop().Y()) * 0.5,
-      //                                   (hit.GetStart().Z() + hit.GetStop().Z()) * 0.5);
-
-      //     nav->FindNode(hit_mid_point.x(), hit_mid_point.y(), hit_mid_point.z());
-
-      //     geo_path node_path(nav->GetPath());
-      //     geo_path partial_path = node_path - gi.root_path();
-      //     geo_id ID = stt->id(partial_path); 
-
-      //     UFW_DEBUG("Hit at position ({}, {}, {}) is in geometry node {}.", hit_mid_point.X(), hit_mid_point.Y(), hit_mid_point.Z(), partial_path);
-      //     UFW_DEBUG("Corresponding geo_id: subdetector {}, supermodule {}, station {}, straw {}.",
-      //               static_cast<int>(ID.stt.subdetector),
-      //               static_cast<int>(ID.stt.supermodule),
-      //               static_cast<int>(ID.stt.plane),
-      //               static_cast<int>(ID.stt.tube));
-
-      //   //   if(nav->FindNode(hit_mid_point.X(), hit_mid_point.Y(), hit_mid_point.Z())){
-      //   //     geo_path node_path = nav->GetPath();
-      //   //     geo_id ID = tracker_ref.id(node_path); 
-
-      //   //     // UFW_DEBUG("Hit at position ({}, {}, {}) is in geometry node.", hit_mid_point.X(), hit_mid_point.Y(), hit_mid_point.Z());
-      //   //     } else {
-      //   // //       UFW_WARN("Could not find geometry node for hit at position ({}, {}, {}).", hit_mid_point.X(), hit_mid_point.Y(), hit_mid_point.Z());
-      //   // //       continue;
-      //   //     }
-      //     }
-      //   }     
-
-      } else {
-        UFW_ERROR("Unknown tracker subdetector type.");
-      }
-
-
+    } else {
+      UFW_ERROR("Unknown tracker subdetector type.");
+    }
   }
 
   void fast_digi::group_hits_by_tube(std::map<geo_id, std::vector<EDEPHit>>& hits_by_tube, 
@@ -134,22 +79,3 @@ namespace sand::stt {
     
   }
 }
-
-UFW_REGISTER_PROCESS(sand::stt::fast_digi)
-UFW_REGISTER_DYNAMIC_PROCESS_FACTORY(sand::stt::fast_digi)
-
-
-//}
-// UFW_DEBUG("Trajectory corresponds to STT subdetector.");
-// for(auto t=0; t<stt->stations().size();t++){
-//   const auto * station_ptr = stt->stations().at(t).get();
-//   if(auto* stt_station = static_cast<const sand::geoinfo::stt_info::station*>(station_ptr)){
-//     UFW_DEBUG("STT station {} with {} wires.", t, stt_station->wires.size());
-//     for(auto w=0; w<stt_station->wires.size();w++){
-//       const auto * wire_ptr = stt_station->wires.at(w).get();
-//       if(auto* stt_wire_ptr = static_cast<const sand::geoinfo::stt_info::wire*>(wire_ptr)){
-//         UFW_DEBUG("STT wire {} with max radius {}.", w, stt_wire_ptr->max_radius);
-//       }
-//     }
-//   }
-// }
