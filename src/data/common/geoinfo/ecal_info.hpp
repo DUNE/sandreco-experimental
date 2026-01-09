f2def44dc114848b91c155cc5d8056685cf848ac
Matteo Tenti <matteo.tenti@gmail.com>
2026-01-09 11:46:50 +0100

    first implementation of fiber struct

--- src/data/common/geoinfo/ecal_info.hpp
+++ src/data/common/geoinfo/ecal_info.hpp
@@ -6,6 +6,34 @@ namespace sand {
 
   class geoinfo::ecal_info : public subdetector_info {
    public:
+    
+    // fiber
+    struct fiber {
+      // attenuation
+      double attenuation_length_1; // cm
+      double attenuation_length_2; // cm
+      double fraction;
+
+      // scintillation
+      double scintillation_constant_1; // ns
+      double scintillation_constant_2;
+
+      // light velocity
+      double light_velocity; // m/ns
+
+      constexpr fiber(double atl2)
+          : attenuation_length_1(50.0),
+            attenuation_length_2(atl2),
+            fraction(0.35),
+            scintillation_constant_1(3.8),
+            scintillation_constant_2(0.588),
+            light_velocity(1./5.85) {}
+    };
+
+    inline constexpr fiber_plane12 = fiber(430.0);
+    inline constexpr fiber_plane3  = fiber(380.0);
+    inline constexpr fiber_plane45 = fiber(330.0);    
+    
     enum class cell_type { barrel, endcap };
 
     struct cell {
