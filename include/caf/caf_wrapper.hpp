32110331580b348dc4f52da5a7ecfc1292d62151
Paolo Forni <paolo.peforni@gmail.com>
2026-01-20 13:23:12 +0000

    Revert caf_wrapper to managed data type

--- include/caf/caf_wrapper.hpp
+++ include/caf/caf_wrapper.hpp
@@ -9,38 +9,14 @@
 
 #include <duneanaobj/StandardRecord/StandardRecord.h>
 
-#include <map>
-#include <memory>
-
 namespace sand::caf {
 
   struct caf_wrapper
     : public ::caf::StandardRecord
-    , public ufw::data::base<ufw::data::complex_tag, ufw::data::instanced_tag, ufw::data::context_tag> {
-    caf_wrapper() = default;
-  };
+    , public ufw::data::base<ufw::data::managed_tag, ufw::data::instanced_tag, ufw::data::context_tag> {};
 
 } // namespace sand::caf
 
-UFW_DECLARE_COMPLEX_DATA(sand::caf::caf_wrapper);
-
-// Factory for caf_wrapper - manages instances per context
-template <>
-class ufw::data::factory<sand::caf::caf_wrapper> {
- public:
-  explicit factory(const ufw::config&) {}
-
-  sand::caf::caf_wrapper& instance(ufw::context_id ctx) {
-    auto it = instances_.find(ctx);
-    if (it == instances_.end()) {
-      instances_[ctx] = std::make_unique<sand::caf::caf_wrapper>();
-      it = instances_.find(ctx);
-    }
-    return *it->second;
-  }
-
- private:
-  std::map<ufw::context_id, std::unique_ptr<sand::caf::caf_wrapper>> instances_;
-};
+UFW_DECLARE_MANAGED_DATA(sand::caf::caf_wrapper);
 
 #endif // SANDRECO_CAF_WRAPPER_HPP
