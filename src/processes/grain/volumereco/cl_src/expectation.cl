84fea9aa30780f6fbbf9a3f88d2fb9e5cc46b387
FelixofRivia <filippomei99@gmail.com>
2026-02-11 10:42:20 +0100

    fixed

--- src/processes/grain/volumereco/cl_src/expectation.cl
+++ src/processes/grain/volumereco/cl_src/expectation.cl
@@ -1,38 +1,16 @@
-CL_KERNEL(void expectation(const transform_t camera_transform, int camera_id, __global const rect_f* mask_rects,
-                            const float z_mask, __global const rect_f* sensor_rects, const float z_sensors,
-                            __global frustum_t* frustums) {
-  const int mask_rect_id  = get_global_id(0);
-  const int sens_rect_id  = get_global_id(1);
-  const int output_id     = sens_rect_id * get_global_size(0) + mask_rect_id;
-  const float2 zo_mask    = (float2)(z_mask, 1.f);
-  const float2 zo_sensors = (float2)(z_sensors, 1.f);
+CL_KERNEL(void expectation(__global const float* sys_mat, __global const float* sensitivity_inv,  uint3 grid, __global const float* previous_iteration_score,  __global float* expectation_result) {
+    //sensor index
+    const int s_idx = get_global_id(0);
+    const int s_size = get_global_size(0);
+    
+    float e_res = 0.f;
+    int sm_idx;
+    float pde = 1.f;
 
-  // find corners of the mask opening in fiducial frame
-  rect_f rect      = mask_rects[mask_rect_id];
-  float4 hole_01_g = transform4(camera_transform, (float4)(rect.left, rect.top, zo_mask));
-  float4 hole_10_g = transform4(camera_transform, (float4)(rect.right, rect.bottom, zo_mask));
-  float4 hole_11_g = transform4(camera_transform, (float4)(rect.right, rect.top, zo_mask));
-  float4 hole_00_g = transform4(camera_transform, (float4)(rect.left, rect.bottom, zo_mask));
-  float4 hole_mid_g =
-      transform4(camera_transform, (float4)((rect.left + rect.right) / 2.f, (rect.top + rect.bottom) / 2.f, zo_mask));
+    for (uint vox_idx = 0; vox_idx < grid.x * grid.y * grid.z; ++vox_idx)  {
+          sm_idx = vox_idx * s_size + s_idx;
+          e_res += pde * sys_mat[sm_idx] * sensitivity_inv[vox_idx] * previous_iteration_score[vox_idx];
+        }
+    expectation_result[s_idx] = (e_res <= 0.f) ? 0.f : 1.f / e_res;       
 
-  // find corners of the active pixel area in fiducial frame
-  rect              = sensor_rects[sens_rect_id];
-  float4 sens_00_g  = transform4(camera_transform, (float4)(rect.left, rect.bottom, zo_sensors));
-  float4 sens_01_g  = transform4(camera_transform, (float4)(rect.left, rect.top, zo_sensors));
-  float4 sens_10_g  = transform4(camera_transform, (float4)(rect.right, rect.bottom, zo_sensors));
-  float4 sens_11_g  = transform4(camera_transform, (float4)(rect.right, rect.top, zo_sensors));
-  float4 sens_mid_g = transform4(camera_transform,
-                                 (float4)((rect.left + rect.right) / 2.f, (rect.top + rect.bottom) / 2.f, zo_sensors));
-
-  // compute the four limiting planes expressed as point + normal (pointing inside frustum) in fiducial frame
-  frustums[output_id].top_o = hole_00_g;
-  frustums[output_id].top_n = normalize(cross(sens_11_g - hole_00_g, sens_01_g - hole_10_g));
-  frustums[output_id].rgt_o = hole_10_g;
-  frustums[output_id].rgt_n = normalize(cross(sens_01_g - hole_10_g, sens_00_g - hole_11_g));
-  frustums[output_id].bot_o = hole_11_g;
-  frustums[output_id].bot_n = normalize(cross(sens_00_g - hole_11_g, sens_10_g - hole_01_g));
-  frustums[output_id].lft_o = hole_01_g;
-  frustums[output_id].lft_n = normalize(cross(sens_10_g - hole_01_g, sens_11_g - hole_00_g));
-  frustums[output_id].idx   = (int3)(camera_id, mask_rect_id, sens_rect_id);
 })
--- src/processes/grain/volumereco/volumereco.cpp
+++ src/processes/grain/volumereco/volumereco.cpp
@@ -9,6 +9,8 @@
 #include <chrono>
 #include <random>
 #include <string>
+#include <algorithm>
+#include <functional>
 
 #include <geoinfo/grain_info.hpp>
 #include <hdf5/hdf5.hpp>
@@ -27,6 +29,8 @@ namespace sand::grain {
    private:
     void configure_expectation(cl::platform& platform);
     void configure_maximization(cl::platform& platform);
+    template <typename T>
+    std::vector<T> get_sensitivity_from_system_matrix(const std::vector<T>& system_matrix, sand::hdf5::ndarray::ndrange dimensions);
     static constexpr size_t s_max_platforms = 4;
     float m_voxel_size;
     cl::Program m_expectation_program;
@@ -41,6 +45,27 @@ namespace sand::grain {
     std::vector<cl::buffer> m_previous_amplitude_buffers; // One per GPU
   };
 
+  template <typename T>
+  std::vector<T> volumereco::get_sensitivity_from_system_matrix(const std::vector<T>& system_matrix, sand::hdf5::ndarray::ndrange dimensions)
+  {
+      assert(system_matrix.size() == dimensions.flat_size());
+      std::vector<T> sensitivity_matrix(dimensions[0]*dimensions[1]*dimensions[2], T{});
+
+      for (std::size_t d0 = 0; d0 < dimensions[0]; ++d0) {
+          for (std::size_t d1 = 0; d1 < dimensions[1]; ++d1) {
+              for (std::size_t d2 = 0; d2 < dimensions[2]; ++d2) {
+                  T sum = T{};
+                  std::size_t voxel_index = (d0 * dimensions[1] + d1) * dimensions[2] + d2;
+                  for (std::size_t d3 = 0; d3 < dimensions[3]; ++d3) {
+                      sum += system_matrix[voxel_index * dimensions[3] + d3];
+                  }
+                  sensitivity_matrix[voxel_index] = sum;
+              }
+          }
+      }
+      return sensitivity_matrix;
+  }
+
   void volumereco::configure_expectation(cl::platform& platform) {
     const char* expectation_kernel_src =
 #include "cl_src/expectation.cl"
@@ -54,29 +79,64 @@ namespace sand::grain {
 #include "cl_src/maximization.cl"
         ;
     platform.build_program(m_maximization_program, maximization_kernel_src);
-    m_maximization_kernel = cl::Kernel(m_maximization_program, "solidangle");
+    m_maximization_kernel = cl::Kernel(m_maximization_program, "maximization");
   }
 
   void volumereco::configure(const ufw::config& cfg) {
     process::configure(cfg);
     m_voxel_size = cfg.at("voxel_size");
     auto& platform   = instance<cl::platform>();
-    // configure_expectation(platform);
-    // configure_maximization(platform);
+    configure_expectation(platform);
+    configure_maximization(platform);
 
     auto& array = instance<sand::hdf5::ndarray>("angle_reader");
-    size_t angle_size = array.range(array.datasets().front()).flat_size();
-    std::vector<float> temp_vector(angle_size, 0.f);
+    const auto angle_dimensions = array.range(array.datasets().front());
+    const size_t angle_size = angle_dimensions.flat_size();
+    const size_t sensitivity_size = angle_dimensions[0]*angle_dimensions[1]*angle_dimensions[2];
+
+    std::vector<float> temp_system(angle_size, 0.f);
+    std::vector<float> temp_sensitivity(sensitivity_size, 0.f);
     for (const auto& camera : array.datasets()) {
       UFW_DEBUG("Loading camera {}", camera);
-      array.read(camera, temp_vector);
-      auto& buf = m_system_matrix_buffers[camera];
-      buf.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY>(
-          platform.context(), angle_size * sizeof(float), temp_vector.data());
+      array.read(camera, temp_system);
+      auto& system_buf = m_system_matrix_buffers[camera];
+      system_buf.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY>(
+          platform.context(), angle_size * sizeof(float), temp_system.data());
+      std::transform(temp_sensitivity.begin(), temp_sensitivity.end(),
+                   get_sensitivity_from_system_matrix<float>(temp_system, angle_dimensions).begin(),
+                   temp_sensitivity.begin(),
+                   std::plus<float>());
+    }
+
+    const size_t n_devices = platform.devices().size();
+    UFW_DEBUG("n_devices: {}", n_devices);
+    m_sensitivity_matrix_buffers = std::vector<sand::cl::buffer>(n_devices);
+    m_image_buffers = std::vector<sand::cl::buffer>(n_devices);
+    m_expectation_buffers = std::vector<sand::cl::buffer>(n_devices);
+    m_maximization_buffers = std::vector<sand::cl::buffer>(n_devices);
+    m_previous_amplitude_buffers = std::vector<sand::cl::buffer>(n_devices);
+
+    for (size_t i_device; i_device < n_devices; ++i_device) {
+      UFW_DEBUG("Creating buffers on device {}", i_device);
+      auto& sensitivity_buf = m_sensitivity_matrix_buffers[i_device];
+      sensitivity_buf.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY>(
+          platform.context(), sensitivity_size * sizeof(float), temp_sensitivity.data());
+
+      auto& image_buf = m_image_buffers[i_device];
+      image_buf.allocate<CL_MEM_READ_ONLY>(platform.context(), camera_height * camera_width * sizeof(float));
+
+      auto& expectation_buf = m_expectation_buffers[i_device];
+      expectation_buf.allocate<CL_MEM_READ_ONLY>(platform.context(), sensitivity_size * sizeof(float));
+
+      auto& maximization_buf = m_maximization_buffers[i_device];
+      maximization_buf.allocate<CL_MEM_READ_ONLY>(platform.context(), sensitivity_size * sizeof(float));
+
+      auto& previous_amplitude_buf = m_previous_amplitude_buffers[i_device];
+      previous_amplitude_buf.allocate<CL_MEM_READ_ONLY>(platform.context(), sensitivity_size * sizeof(float));
     }
   }
 
-  volumereco::volumereco() : process({{"images", "sand::grain::images"}}, {{"voxels", "sand::grain::voxel_array<float>"}}) {
+  volumereco::volumereco() : process({{"images", "sand::grain::images"}}, {}) {
     UFW_DEBUG("Creating a volumereco process at {}.", fmt::ptr(this));
   }
 
