3f06891407becb9a0778800cce2db08d7fb7d58b
Nicolò Tosi <ntosi@bo.infn.it>
2026-03-30 15:28:56 +0200

    Ported Tracker

--- src/processes/stt/fast_digi/stt_fast_digi.cpp
+++ src/processes/stt/fast_digi/stt_fast_digi.cpp
@@ -118,8 +118,8 @@ namespace sand::stt {
     std::normal_distribution<double> gaussian_error(0.0, m_sigma_tdc); //FIXME should be member
     auto ran = gaussian_error(random_engine());
     //FIXME replace 200 with maximum drift + signal time
-    reco::digi<>::time trange{wire_time - 200., wire_time, wire_time + 5. * m_sigma_tdc};
-    tracker::digi::signal signal{reco::digi<>{channel, trange, reco::digi<>::source::sim}, wire_time + ran, edep_total};
+    reco::digi<>::time trange{wire_time - 200., wire_time + ran, wire_time + 5. * m_sigma_tdc};
+    tracker::digi::signal signal(channel, trange, edep_total);
 
     UFW_DEBUG("  Created signal: Channel(subdetector {}, channel {}), TDC = {}, ADC = {}",
               static_cast<int>(signal.channel().subdetector), static_cast<int>(signal.channel().channel), signal.tdc,
