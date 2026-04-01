2423bb0b1abee5f3a5f08237a426f9e4e3b1cae3
giacomo-santoni <giacomo.santoni12@gmail.com>
2026-04-01 12:32:49 +0000

    fill SRTrack with smeared energy

--- src/processes/fake_reco/caf_handlers/caf_filler.hpp
+++ src/processes/fake_reco/caf_handlers/caf_filler.hpp
@@ -86,6 +86,10 @@ namespace sand {
 
     [[nodiscard]] static ::caf::SRTrack from_true(const ::caf::SRTrueParticle& true_part,
                                                   const ::caf::TrueParticleID& id);
+
+    [[nodiscard]] static ::caf::SRTrack from_true_with_mu_smearing(const ::caf::SRTrueParticle& true_part,
+                                                                      const ::caf::TrueParticleID& id, 
+                                                                          const EDEPTrajectory& true_part_trj);
   };
 
   /// @brief Fills SRShower from truth (fake reconstruction)
--- src/processes/fake_reco/caf_handlers/caf_filler_reco.cpp
+++ src/processes/fake_reco/caf_handlers/caf_filler_reco.cpp
@@ -75,6 +75,35 @@ namespace sand {
     return track;
   }
 
+    ::caf::SRTrack CAFFiller<::caf::SRTrack>::from_true_with_mu_smearing(const ::caf::SRTrueParticle& true_part,
+                                                                    const ::caf::TrueParticleID& id, const EDEPTrajectory& true_part_trj) {
+    // start by filling SRRecoParticle with all fields from truth
+    auto reco = CAFFiller<::caf::SRRecoParticle>::from_true(true_part, id);
+    // start by filling SRTrack with all fields from truth
+    auto reco_track = CAFFiller<::caf::SRTrack>::from_true(true_part, id);
+
+    // return from_true for all particle except muons (temporarily)
+    if(std::abs(true_part.pdg)!=13) { return reco_track; }
+
+    // for muons in the drift tracker (temporarily) smear according to the Gluckstern formula
+    const auto& hit_map = true_part_trj.GetHitMap();
+    const auto& it = hit_map.find(component::DRIFT);
+    if(it == hit_map.end()) { return reco_track; }
+    const auto& hit_vec = it->second;
+
+    const auto gluckstern_helper = smearing::gluckstern::GlucksternSmearing{hit_vec};
+
+    // apply the measurement resolution smearing
+    reco.p = gluckstern_helper.apply_smearing(true_part.p);
+
+    // apply smearing on the energy as well, using the smeared momentum and the muon mass
+    const double muon_mass = 105.6583755; // MeV/c^2
+    reco_track.E = std::sqrt(reco.p.x*reco.p.x + reco.p.y*reco.p.y + reco.p.z*reco.p.z + muon_mass*muon_mass);
+    reco_track.Evis = reco_track.E;
+    
+    return reco_track;
+  }
+
   ::caf::SRShower CAFFiller<::caf::SRShower>::from_true(const ::caf::SRTrueParticle& true_part,
                                                         const ::caf::TrueParticleID& id) {
     ::caf::SRShower shower{};
