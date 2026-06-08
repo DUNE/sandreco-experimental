dd126a3c60cd66de0cb334ec77266f2feea5369f
ntosi <ntosi@bo.infn.it>
2026-06-08 19:59:58 +0200

    Skeleton

---
+++ src/data/grain/volumereco_cl_manager/volumereco_cl_manager.cpp
@@ -0,0 +1,9 @@
+#include <volumereco_cl_manager.hpp>
+#include <ocl/ocl.hpp>
+#include <hdf5/hdf5.hpp>
+
+namespace sand::grain {
+
+    volumereco_cl_manager::volumereco_cl_manager(const ufw::config&) {}
+
+} // namespace sand::grain
