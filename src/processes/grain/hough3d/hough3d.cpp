a5eb7fc46472d814b829ca6f7f8f8d68ef65bb44
FelixofRivia <filippomei99@gmail.com>
2026-06-15 17:05:38 +0200

    opencl placeholder

--- src/processes/grain/hough3d/hough3d.cpp
+++ src/processes/grain/hough3d/hough3d.cpp
@@ -10,6 +10,8 @@
 #include <ocl/ocl.hpp>
 #include <hough3d.hpp>
 
+#include <cmath>
+
 namespace sand::grain {
 
   /**
@@ -19,27 +21,40 @@ namespace sand::grain {
    *
    *
    * \subsection Configuration
-   * | Parameter Name           | Type   | Unit            | Required/Default | Description                                                                         |
-   * |--------------------------|--------|-----------------|------------------|-------------------------------------------------------------------------------------|
-   * | `n_versors_in_sphere`    | uint   |                 | Default: 100     | How many versors represent Fibonacci's sphere, for direction binning.               |
-   * | `xy_plane_step`          | double | mm              | Required         | Bin size for xy plane in Hough space.                                               |
-   * | `min_points_per_track`   | uint   |                 | Required         | Minimum number of points to build a track.                                          |
-   * | `max_tracks_per_event`   | uint   |                 | Default: 4       | Maximum number of tracks to search for in one event.                                |
+   * | Parameter Name           | Type   | Unit  | Required/Default | Description                                                                         |
+   * |--------------------------|--------|-------|------------------|-------------------------------------------------------------------------------------|
+   * | `n_versors_in_sphere`    | uint   |       | Default: 100     | How many versors represent Fibonacci's sphere, for direction binning.               |
+   * | `xy_plane_step`          | double | mm    | Required         | Bin size for xy plane in Hough space.                                               |
+   * | `min_points_per_track`   | uint   |       | Required         | Minimum number of points to build a track.                                          |
+   * | `max_tracks_per_event`   | uint   |       | Default: 4       | Maximum number of tracks to search for in one event.                                |
    */
   class hough3d : public ufw::process {
    public:
     hough3d();
     void configure(const ufw::config& cfg) override;
+    void configure_hough_vote(cl::platform& platform);
     void run() override;
 
    private:
     static constexpr size_t s_max_platforms = 4;
     uint m_n_versors_in_sphere;
     double m_xy_plane_step;
+    size_t m_n_xy_bins;
     uint m_min_points_per_track;
     uint m_max_tracks_per_event;
-    std::vector<dir_3d> m_unique_versors;
+    std::vector<cl_float3> m_unique_versors;
+    std::vector<float> m_voting_array;
+    cl::Program m_hough_vote_program;
+    cl::Kernel m_hough_vote_kernel;
   };
+
+  void hough3d::configure_hough_vote(cl::platform& platform) {
+    const char* code_kernel_src =
+#include "cl_src/hough_vote.cl"
+        ;
+    platform.build_program(m_hough_vote_program, code_kernel_src);
+    m_hough_vote_kernel = cl::Kernel(m_hough_vote_program, "hough_vote");
+  }
 
   void hough3d::configure(const ufw::config& cfg) {
     process::configure(cfg);
@@ -49,12 +64,19 @@ namespace sand::grain {
     m_max_tracks_per_event = cfg.value("max_tracks_per_event", 4);
 
     // Binning versors and x'y' plane
-    const std::vector<dir_3d> versors = fibonacci_sphere_versors(m_n_versors_in_sphere);
+    const std::vector<cl_float3> versors = fibonacci_sphere_versors(m_n_versors_in_sphere);
     m_unique_versors = select_unique_versors(versors);
-
     const auto& gi = ufw::context::current()->instance<geoinfo>();
     const dir_3d grain_dimensions = gi.grain().fiducial_bbox();
     const double xy_half_range = grain_dimensions.R();
+    m_n_xy_bins = static_cast<size_t>(std::ceil(2.0 * xy_half_range / m_xy_plane_step));
+
+    m_voting_array.assign(m_unique_versors.size() * m_n_xy_bins * m_n_xy_bins, 0.0);
+    UFW_INFO("Size of voting array: {}", m_voting_array.size());
+    UFW_INFO("Memory size of voting array: {} kB", static_cast<float>(sizeof(m_voting_array)) / 1024.0);
+
+    auto& platform = instance<cl::platform>();
+    configure_hough_vote(platform);
   }
 
   hough3d::hough3d() : process({{"point_cloud", "sand::grain::point_cloud"}}, {}) {
@@ -65,6 +87,14 @@ namespace sand::grain {
     UFW_DEBUG("Running a hough3d process at {}.", fmt::ptr(this));
     auto& platform = instance<cl::platform>();
     const auto& point_cloud_in  = get<point_cloud>("point_cloud");
+    // Loop on events in a spill
+    for (const auto& ev_points : point_cloud_in.points) {
+      std::vector<cl_float4> cl_points = point_cloud_to_float4(ev_points);
+      UFW_INFO("Processing {} points", cl_points.size());
+      cl::buffer buf_points;
+      buf_points.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE>(
+          platform.context(), cl_points.size() * sizeof(cl_float4), cl_points.data());
+    }
 
   }
 } // namespace sand::grain
--- src/processes/grain/hough3d/hough3d.hpp
+++ src/processes/grain/hough3d/hough3d.hpp
@@ -4,12 +4,13 @@
 #include <vector>
 
 #include <common/sand.h>
+#include <ocl/ocl.hpp>
 
 namespace sand::grain {
 
     // Evenly distribute point across sphere surface using golden spiral method (Fibonacci's sphere)
-    std::vector<dir_3d> fibonacci_sphere_versors(size_t n_versors) {
-        std::vector<dir_3d> versors;
+    std::vector<cl_float3> fibonacci_sphere_versors(size_t n_versors) {
+        std::vector<cl_float3> versors;
         versors.reserve(n_versors);
 
         if (n_versors == 0) {
@@ -17,13 +18,13 @@ namespace sand::grain {
             return versors;
         } 
 
-        const double golden_angle = M_PI * (3.0 - std::sqrt(5.0));
+        const float golden_angle = M_PI * (3.0 - std::sqrt(5.0));
 
         for (size_t i = 0; i < n_versors; ++i)
         {
-            const double z = 1.0 - 2.0 * (i + 0.5) / n_versors;
-            const double r = std::sqrt(1.0 - z * z);
-            const double theta = golden_angle * i;
+            const float z = 1.0 - 2.0 * (i + 0.5) / n_versors;
+            const float r = std::sqrt(1.0 - z * z);
+            const float theta = golden_angle * i;
 
             versors.push_back({r * std::cos(theta), r * std::sin(theta), z});
         }
@@ -32,19 +33,29 @@ namespace sand::grain {
     }
 
     // Remove redundant symmetries
-    std::vector<dir_3d> select_unique_versors(const std::vector<dir_3d>& versors) {
-        std::vector<dir_3d> unique_versors;
+    std::vector<cl_float3> select_unique_versors(const std::vector<cl_float3>& versors) {
+        std::vector<cl_float3> unique_versors;
         unique_versors.reserve(versors.size());
 
         constexpr double eps = 1e-12;
 
         for (const auto& v : versors) {
-            if (v.z() > 0 || (std::abs(v.z()) < eps && v.y() > 0) || (std::abs(v.z()) < eps && std::abs(v.y()) < eps && v.x() > 0)) {
+            if (v.s[2] > 0 || (std::abs(v.s[2]) < eps && v.s[1] > 0) || (std::abs(v.s[2]) < eps && std::abs(v.s[1]) < eps && v.s[0] > 0)) {
                 unique_versors.push_back(v);
             }
         }
 
         return unique_versors;
+    }
+
+    // Convert 3d point cloud to cl_float4 for GPU processing
+    std::vector<cl_float4> point_cloud_to_float4(const std::vector<point_cloud::point>& points) {
+        std::vector<cl_float4> converted_points;
+        converted_points.reserve(points.size());
+        for (const auto& p : points) {
+            converted_points.push_back({static_cast<float>(p.position.x()), static_cast<float>(p.position.y()), static_cast<float>(p.position.z()), static_cast<float>(p.amplitude)});
+        }
+        return converted_points;
     }
 
 
