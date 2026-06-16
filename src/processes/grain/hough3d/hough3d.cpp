036b9f30484047f1aa4e1ba5a7d3b7ca16351a80
FelixofRivia <filippomei99@gmail.com>
2026-06-16 16:47:07 +0200

    update

--- src/processes/grain/hough3d/hough3d.cpp
+++ src/processes/grain/hough3d/hough3d.cpp
@@ -33,19 +33,24 @@ namespace sand::grain {
     hough3d();
     void configure(const ufw::config& cfg) override;
     void configure_hough_vote(cl::platform& platform);
+    void configure_distance(cl::platform& platform);
     void run() override;
 
    private:
     static constexpr size_t s_max_platforms = 4;
     uint m_n_versors_in_sphere;
-    double m_xy_plane_step;
+    float m_xy_plane_step;
     size_t m_n_xy_bins;
     uint m_min_points_per_track;
     uint m_max_tracks_per_event;
     std::vector<cl_float3> m_unique_versors;
-    std::vector<float> m_voting_array;
+    std::vector<uint> m_voting_array;
     cl::Program m_hough_vote_program;
     cl::Kernel m_hough_vote_kernel;
+    cl::Program m_distance_program;
+    cl::Kernel m_distance_kernel;
+    cl::buffer m_buf_unique_versors;
+    cl::buffer m_buf_voting_array;
   };
 
   void hough3d::configure_hough_vote(cl::platform& platform) {
@@ -54,6 +59,14 @@ namespace sand::grain {
         ;
     platform.build_program(m_hough_vote_program, code_kernel_src);
     m_hough_vote_kernel = cl::Kernel(m_hough_vote_program, "hough_vote");
+  }
+
+  void hough3d::configure_distance(cl::platform& platform) {
+    const char* code_kernel_src =
+#include "cl_src/distance.cl"
+        ;
+    platform.build_program(m_distance_program, code_kernel_src);
+    m_distance_kernel = cl::Kernel(m_distance_program, "hough_distance");
   }
 
   void hough3d::configure(const ufw::config& cfg) {
@@ -71,12 +84,16 @@ namespace sand::grain {
     const double xy_half_range = grain_dimensions.R();
     m_n_xy_bins = static_cast<size_t>(std::ceil(2.0 * xy_half_range / m_xy_plane_step));
 
-    m_voting_array.assign(m_unique_versors.size() * m_n_xy_bins * m_n_xy_bins, 0.0);
+    m_voting_array.assign(m_unique_versors.size() * m_n_xy_bins * m_n_xy_bins, 0);
     UFW_INFO("Size of voting array: {}", m_voting_array.size());
-    UFW_INFO("Memory size of voting array: {} kB", static_cast<float>(sizeof(m_voting_array)) / 1024.0);
+    UFW_INFO("Memory size of voting array: {} kB", static_cast<float>(sizeof(uint) * m_voting_array.size()) / 1024.0);
 
     auto& platform = instance<cl::platform>();
     configure_hough_vote(platform);
+    configure_distance(platform);
+
+    m_buf_unique_versors.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY>(platform.context(), m_unique_versors.size() * sizeof(cl_float3), m_unique_versors.data());
+    m_buf_voting_array.allocate<CL_MEM_READ_WRITE>(platform.context(), m_voting_array.size() * sizeof(uint));
   }
 
   hough3d::hough3d() : process({{"point_cloud", "sand::grain::point_cloud"}}, {}) {
@@ -92,8 +109,67 @@ namespace sand::grain {
       std::vector<cl_float3> cl_points = point_cloud_to_float3(ev_points);
       UFW_INFO("Processing {} points", cl_points.size());
       cl::buffer buf_points;
-      buf_points.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE>(
-          platform.context(), cl_points.size() * sizeof(cl_float4), cl_points.data());
+      buf_points.allocate<CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE>(platform.context(), cl_points.size() * sizeof(cl_float3), cl_points.data());
+
+      // Reset voting array
+      std::fill(m_voting_array.begin(), m_voting_array.end(), 0);
+      m_buf_voting_array.write(m_voting_array.data(), platform.queues().front());
+
+      platform.queues().front().finish();
+
+      // set kernel args
+      try {
+        m_hough_vote_kernel.setArg(0, buf_points);
+        m_hough_vote_kernel.setArg(1, m_buf_unique_versors);
+        m_hough_vote_kernel.setArg(2, m_buf_voting_array);
+        m_hough_vote_kernel.setArg(3, m_xy_plane_step);
+        m_hough_vote_kernel.setArg(4, static_cast<int>(m_n_xy_bins));
+      } catch (const cl::Error& e) {
+        UFW_WARN("OpenCL hough vote Program Kernel setArg: {} ({})", e.what(), e.err());
+        throw;
+      }
+
+      platform.queues().front().enqueueNDRangeKernel(m_hough_vote_kernel, cl::NullRange, cl::NDRange(cl_points.size(), m_unique_versors.size()), cl::NullRange, nullptr, nullptr);
+      platform.queues().front().finish();
+
+      void* tmp_ptr = m_voting_array.data();
+      m_buf_voting_array.read(tmp_ptr, platform.queues().front(), 0, -1, {});
+      platform.queues().front().finish();
+
+      // Find maximum
+      const size_t max_votes_index =  std::distance(m_voting_array.begin(),  std::max_element(m_voting_array.begin(), m_voting_array.end()));
+      const uint vote_count = m_voting_array[max_votes_index];
+      const size_t max_versor_index = max_votes_index / (m_n_xy_bins * m_n_xy_bins);
+      const size_t remainder = max_votes_index % (m_n_xy_bins * m_n_xy_bins);
+      const size_t max_x_index = remainder / m_n_xy_bins;
+      const size_t max_y_index = remainder % m_n_xy_bins;
+
+      const auto& max_versor = m_unique_versors[max_versor_index];
+      const float max_x = (0.5 + max_x_index - m_n_xy_bins / 2) * m_xy_plane_step;
+      const float max_y = (0.5 + max_y_index - m_n_xy_bins / 2) * m_xy_plane_step;
+      const cl_float3 line_point{max_x, max_y, 0.0};
+
+      UFW_DEBUG("Index of voting array maximum: {}, votes: {}, (versor,x,y): (({},{},{}),{},{})", max_votes_index, vote_count, max_versor.s[0], max_versor.s[1], max_versor.s[2], max_x, max_y);
+
+      // Find points close to line
+      cl::buffer buf_distances;
+      buf_distances.allocate<CL_MEM_READ_WRITE>(platform.context(), cl_points.size() * sizeof(float));
+      // set kernel args
+      try {
+        m_distance_kernel.setArg(0, buf_points);
+        m_distance_kernel.setArg(1, max_versor);
+        m_distance_kernel.setArg(2, line_point);
+        m_distance_kernel.setArg(3, buf_distances);
+      } catch (const cl::Error& e) {
+        UFW_WARN("OpenCL distance Program Kernel setArg: {} ({})", e.what(), e.err());
+        throw;
+      }
+
+      platform.queues().front().enqueueNDRangeKernel(m_distance_kernel, cl::NullRange, cl::NDRange(cl_points.size()), cl::NullRange, nullptr, nullptr);
+      platform.queues().front().finish();
+
+      //std::vector<float> points_distances(cl_points.size(),0);
+
     }
 
   }
