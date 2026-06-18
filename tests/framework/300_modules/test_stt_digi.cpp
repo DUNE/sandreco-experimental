537701e245a5e192ffc6a5c43b436e3e291ff03c
fbattist <federico.battisti@bo.infn.it>
2026-06-18 11:23:16 +0200

    Added tdc test

--- tests/framework/300_modules/test_stt_digi.cpp
+++ tests/framework/300_modules/test_stt_digi.cpp
@@ -22,13 +22,20 @@ namespace sand::test {
 			void run() override;
 			void analyze_stt_digi();
 			std::unordered_map<int, const EDEPHit*> get_hit_id_map_in_straws();
+			double get_time_range(const sand::geoinfo::tracker_info::wire& wire);
 			void check_truth_matching(const std::unordered_map<int, const EDEPHit*>& all_straw_hits);
 
   	private:
+			double m_drift_velocity;
+			double m_wire_velocity;
+			double m_sigma_tdc;
   };
 
   void test_stt_digi::configure(const ufw::config& cfg) {
     UFW_DEBUG("test_stt_digi configured at: {}", fmt::ptr(this));
+		m_drift_velocity = cfg.at("drift_velocity");
+    m_wire_velocity  = cfg.at("wire_velocity");
+    m_sigma_tdc      = cfg.at("sigma_tdc");
   }
 
   test_stt_digi::test_stt_digi() : process({{"digi", "sand::tracker::digi"}}, {}) {
@@ -40,6 +47,7 @@ namespace sand::test {
     analyze_stt_digi();
   }
 
+  /// @brief Analyze the tracker digi signals and check their truth matching with the EDEPHit information.
   void test_stt_digi::analyze_stt_digi() {
     const auto& digi = get<sand::tracker::digi>("digi");
     const auto& gi   = get<geoinfo>();
@@ -66,6 +74,8 @@ namespace sand::test {
 		UFW_DEBUG("============== END OF DIGI ANALYSIS ==============");
   }
 
+  /// @brief Get a map of all straw hits indexed by their IDs
+  /// @return Map of straw hit IDs to pointers to the corresponding EDEPHit objects
   std::unordered_map<int, const EDEPHit*> test_stt_digi::get_hit_id_map_in_straws() {
     const auto& tree = get<sand::edep_reader>();
     std::unordered_map<int, const EDEPHit*> all_straw_hits;
@@ -80,8 +90,17 @@ namespace sand::test {
     return all_straw_hits;
   }
 
-  /// @brief Check that the true hits associated with each tracker signal match the expected energy deposit 
-	///				 and are within the maximum drift radius of the corresponding wire.
+	/// @brief Get the maximum time range for signal formation on a given wire
+	/// @param wire 
+	/// @return Maximum time range for signal formation on the wire, considering drift time, wire propagation time, and TDC resolution.
+	double test_stt_digi::get_time_range(const sand::geoinfo::tracker_info::wire& wire){
+		double max_drift_time = wire.max_radius / m_drift_velocity;
+		double max_wire_time = wire.length() / m_wire_velocity;
+		return max_drift_time + max_wire_time + 5 * m_sigma_tdc;
+	}
+
+  /// @brief Check that the true hits associated with each tracker signal match the expected energy deposit, 
+	///				 the time matches the maximum signal formation time range and are within the maximum drift radius of the corresponding wire.
   /// @param all_straw_hits 
   void test_stt_digi::check_truth_matching(const std::unordered_map<int, const EDEPHit*>& all_straw_hits) {
 
@@ -99,6 +118,8 @@ namespace sand::test {
                 channel_wire.hv, channel_wire.max_radius);
 
       double total_energy_deposit = 0.0;
+			double smallest_time = std::numeric_limits<double>::max();
+
       for (const auto& true_hit : signal.true_hits()) {
         UFW_DEBUG("  True hit index: {}", true_hit.get());
         auto it = all_straw_hits.find(true_hit.get());
@@ -117,12 +138,18 @@ namespace sand::test {
           UFW_ERROR("Hit is outside the maximum drift radius");
 
         total_energy_deposit += hit->GetEnergyDeposit();
+				smallest_time = std::min(smallest_time, hit->GetStart().T());
       }
 
-      if (std::abs(total_energy_deposit - signal.adc()) > 1e-3)
+      if (std::abs(total_energy_deposit - signal.adc()) > 1e-7)
         UFW_ERROR("Energy mismatch: true hits sum = {}, ADC = {}", total_energy_deposit, signal.adc());
       else
         UFW_DEBUG("Energy deposit matches ADC");
+
+			if (abs(smallest_time - signal.tdc()) > get_time_range(channel_wire))
+        UFW_ERROR("Smallest time is outside the expected range");
+			else
+				UFW_DEBUG("Smallest time is within the expected range");
     }
   }
 } // namespace sand::test
