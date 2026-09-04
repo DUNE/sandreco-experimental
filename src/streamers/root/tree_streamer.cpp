eea29a02169e61f62df42b63ea84fbcfc05d57b0
Valentina Cicero <cicero@bo.infn.it>
2026-09-04 17:24:54 +0200

    hope that this fix works...

--- src/streamers/root/tree_streamer.cpp
+++ src/streamers/root/tree_streamer.cpp
@@ -9,7 +9,6 @@
 #include <ufw/factory.hpp>
 
 #include <tree_streamer.hpp>
-#include <include_paths.h>
 
 #define UFW_IMPLEMENT_STREAMER_FOR_TYPE(type) UFW_DECLARE_RTTI(type)
 #include <tree_streamer_types.hpp>
@@ -19,8 +18,6 @@ namespace sand::root {
 
   tree_streamer::tree_streamer() : m_file(nullptr), m_tree(nullptr), m_branchaddr(nullptr), m_id(), m_last_entry(-1) {
     m_id_ptr = &m_id;
-    gInterpreter->AddIncludePath(PUBLIC_HEADERS_PATH);
-    gInterpreter->AddIncludePath(PUBLIC_HEADERS_PATH "/common");
   }
 
   tree_streamer::~tree_streamer() {
