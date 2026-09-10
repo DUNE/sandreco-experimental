488f96d7a220739187297e5a1a09d5cf81a0caa4
FelixofRivia <filippomei99@gmail.com>
2026-09-10 11:04:17 +0200

    fix docs

--- src/processes/grain/fit_cluster/fit_cluster.cpp
+++ src/processes/grain/fit_cluster/fit_cluster.cpp
@@ -35,11 +35,6 @@ namespace sand::grain {
    * | `fit_step_size`           | double |       | Default: 0.01    | Step size for line fit.                                               |
    * | `use_weights`             | bool   |       | Default: true    | Flag to use points amplitude as weights in fit minimizer.             |
    *
-   * \subsection Dependencies
-   * | Type            | Comment  |
-   * |-----------------|----------|
-   * | `sand::geoinfo` | Geometry |
-   *
    * \subsection Requirements
    * |  Name               | Type                          | Comment                           |
    * |---------------------|-------------------------------|-----------------------------------|
